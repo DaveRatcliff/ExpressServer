c2b4a74d510f99c057e4db06a0961019328c7b1c
DaveRatcliff <37420330+DaveRatcliff@users.noreply.github.com>
2018-03-28 11:23:00 -0500

    Added comments/axios

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ app.get('http://127.0.0.1/posts', (req, res) => res.send())
 
 async function getPost() {
     try {
-      const response = await axios.get(' https://jsonplaceholder.typicode.com/posts');
+      const response = await 
+      axios.get(' https://jsonplaceholder.typicode.com/posts');
       console.log(response);
     } catch (error) {
       console.error(error);
