666b3ae166186c50de802686f909ba277c83261e
Wes Ratcliff <wes.ratcliff@gmail.com>
2018-03-28 09:37:05 -0700

    feat: Added hints

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
+const axios = require('axios')
 const express = require('express')
 const app = express()
 
-app.get('http://127.0.0.1/posts', (req, res) => res.send())
+app.get('/posts', (req, res) => { // Research how to use async/await with Express
+  // TODO -- await the array of posts (or error) from getPost
+  res.send('Hello!') // TODO -- Use an HTML templating engine to feed in the list of posts and do the basic formatting
+})
+
+app.listen(3000, () => console.log('Listening on port 3000!'))
 
 async function getPost() {
-    try {
-      const response = await 
-      axios.get(' https://jsonplaceholder.typicode.com/posts');
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    console.log(response)
+  } catch (error) {
+    console.error(error)
   }
-
-  //API probably needs to feed into an array?
-  //list is populated by a for loop that cycles through the array?
+  // TODO -- Return result as an array or error
+}
