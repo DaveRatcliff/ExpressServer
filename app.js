419ac72398d4f5d81ecd1b5b41ffb0f898c0650c
DaveRatcliff <37420330+DaveRatcliff@users.noreply.github.com>
2018-03-28 11:21:03 -0500

    Added comments/axios

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('test'))
+app.get('http://127.0.0.1/posts', (req, res) => res.send())
 
-app.listen("http://127.0.0.1/posts", () => console.log('Should point to URL'))
+axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+  //API probably needs to feed into an array?
+  //list is populated by a for loop that cycles through the array?
