788b04959200038a03663fa4ab5b55fa28fa3ee3
DaveRatcliff <37420330+DaveRatcliff@users.noreply.github.com>
2018-03-29 15:16:42 -0500

    Everything but ID working

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const axios = require('axios')
 const express = require('express')
 const handlebars = require('handlebars')
-const expressHandlebars = require("express-handlebars")
+const exphbs  = require('express-handlebars');
 const app = express()
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 const asyncMiddleware = fn =>
   (req, res, next) => {
@@ -14,34 +17,35 @@ app.listen(3000, () => console.log('Listening on port 3000!'))
 
 app.get('/posts', asyncMiddleware( async (req, res, next) => { 
   const posts = await getPosts()
-  return res.render('home', {posts})
+  console.log("got it")
+  return res.render('home', {posts: posts})
 }))
 
 app.get('/posts/:id', asyncMiddleware( async (req, res, next) => { 
   const id = req.params.id
   const post = await getPost(id)
-  return res.render('home', {post})
+  return res.render('post', {post})
 }))
 
 app.get('/api/posts', asyncMiddleware( async (req, res, next) => { 
   const posts = await getPosts()
-  return res.JSON(posts)
+  console.log("got it")
+  return res.json(posts)
 }))
 
-//res.render?
 
 async function getPosts() {
   try {
     const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
     return response.data
   } catch (error) {
-    console.error(error)
+    console.log("somethings wrong")
   }
 }
 
 async function getPost(id) {
   try {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/posts${id}`)
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/posts?id=${id}`)
     return response.data
   } catch (error) {
     console.error(error)
