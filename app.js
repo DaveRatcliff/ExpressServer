7f46fdcc0dbfd47fef89c880ab659363d3d9f4d0
DaveRatcliff <37420330+DaveRatcliff@users.noreply.github.com>
2018-03-29 11:33:45 -0500

    Added almost functioning code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 const express = require('express')
 const handlebars = require('handlebars')
-const handlebarExpress = require("handlebar-express")
+const handlebarExpress = require("express-handlebars")
 const app = express()
 
 const asyncMiddleware = fn =>
@@ -12,19 +12,19 @@ const asyncMiddleware = fn =>
 
 app.listen(3000, () => console.log('Listening on port 3000!'))
 
-app.get('/posts', asyncMiddleware( (req, res, next) => { 
+app.get('/posts', asyncMiddleware( async (req, res, next) => { 
   const posts = await getPosts()
   return res.send(posts)
 }))
 
-app.get('posts/:id', asyncMiddleware( (req, res, next) => { 
+app.get('posts/:id', asyncMiddleware( async (req, res, next) => { 
   const postID = req.params.id
   const post = await getPost(postID)
   return res.send(post)
 }))
 
 
-app.get('api/posts', asyncMiddleware( (req, res, next) => { 
+app.get('api/posts', asyncMiddleware( async (req, res, next) => { 
   const posts = await getPosts()
   return res.JSON(posts)
 }))
